aeb62504cfc50560c9a54c7005369258ec935839
Chris Drake <cjdrake@gmail.com>
2023-10-24 23:33:19 -0400

    Implement clog2 function

--- src/seqlogic/util.py
+++ src/seqlogic/util.py
@@ -14,3 +14,32 @@ def bools2int(*xs: bool) -> int:
     for i, x in enumerate(xs):
         y |= x << i
     return y
+
+
+def clog2(x: int) -> int:
+    """Return the ceiling log base two of an integer ≥ 1.
+
+    This function tells you the minimum dimension of a Boolean space with at
+    least N points.
+
+    For example, here are the values of `clog2(N)` for `1 ≤ N < 18`:
+
+    >>> [clog2(n) for n in range(1, 18)]
+    [0, 1, 2, 2, 3, 3, 3, 3, 4, 4, 4, 4, 4, 4, 4, 4, 5]
+
+    This function is undefined for non-positive integers:
+
+    >>> clog2(0)
+    Traceback (most recent call last):
+        ...
+    ValueError: Expected x ≥ 1, got 0
+    """
+    if x < 1:
+        raise ValueError(f"Expected x ≥ 1, got {x}")
+
+    y = 0
+    shifter = 1
+    while x > shifter:
+        shifter <<= 1
+        y += 1
+    return y
