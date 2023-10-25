ec244a0244474ff4a49d9b44da5dac48f68160ca
Chris Drake <cjdrake@gmail.com>
2023-10-25 17:29:48 -0400

    Add an example FSM test
    Use Python Enum to describe the state variable.
    
    Also move a couple test common items into a common module.

---
+++ tests/common.py
@@ -0,0 +1,145 @@
+"""
+Common code
+
+For now this is just used for testing.
+It might be useful to add to seqlogic library.
+"""
+
+from collections import defaultdict
+from collections.abc import Callable
+
+from seqlogic.logic import logic
+from seqlogic.logicvec import logicvec, xes
+from seqlogic.sim import SimVar, notify, sleep
+
+# [Time][Var] = Val
+waves = defaultdict(dict)
+
+
+class TraceVar(SimVar):
+    """
+    Variable that supports dumping to memory.
+    """
+
+    def __init__(self):
+        super().__init__(value=logic.X)
+        waves[self._sim.time()][self] = self._value
+
+    def update(self):
+        if self.dirty():
+            waves[self._sim.time()][self] = self._next
+        super().update()
+
+    def negedge(self) -> bool:
+        return (self._value is logic.T) and (self._next is logic.F)
+
+    def posedge(self) -> bool:
+        return (self._value is logic.F) and (self._next is logic.T)
+
+
+class TraceVec(SimVar):
+    """
+    Variable that supports dumping to memory.
+    """
+
+    def __init__(self, n: int):
+        super().__init__(value=xes((n,)))
+        waves[self._sim.time()][self] = self._value
+
+    def update(self):
+        if self.dirty():
+            waves[self._sim.time()][self] = self._next
+        super().update()
+
+
+async def reset_drv(
+    reset: TraceVar, init: logic = logic.T, phase1_ticks: int = 1, phase2_ticks: int = 1
+):
+    r"""
+    Simulate a reset signal.
+
+    ‾‾‾‾‾‾‾‾\\________/‾‾‾‾‾‾‾‾
+
+    <phase1> <phase2>
+
+    Args:
+        phase1_ticks: Number of sim ticks in ...
+        phase2_ticks: Number of sim ticks in ...
+
+    Raises:
+        ValueError: An argument has the correct type, but an incorrect value
+    """
+    if phase1_ticks < 0:
+        raise ValueError(f"Expected phase1_ticks ≥ 0, got {phase1_ticks}")
+    if phase2_ticks < 0:
+        raise ValueError(f"Expected phase2_ticks ≥ 0, got {phase2_ticks}")
+
+    # T = 0
+    reset.next = init
+    await sleep(phase1_ticks)
+
+    reset.next = ~reset.value
+    await sleep(phase2_ticks)
+
+    reset.next = ~reset.value
+
+
+async def clock_drv(
+    clock: TraceVar,
+    init: logic = logic.F,
+    shift_ticks: int = 0,
+    phase1_ticks: int = 1,
+    phase2_ticks: int = 1,
+):
+    r"""
+    Simulate a clock signal.
+
+    ________/‾‾‾‾‾‾‾‾\\________/‾‾‾‾‾‾‾‾\\________
+
+    <shift> <phase1> <phase2>
+
+    Args:
+        init: Initial clock signal value
+        shift_ticks: Number of sim ticks to shift the first clock transition
+        phase1_ticks: Number of sim ticks in the clock's active phase
+        phase2_ticks: Number of sim ticks in the clock's passive phase
+
+    The period is phase1_ticks + phase2_ticks
+    The duty cycle is phase1_ticks / (phase1_ticks + phase2_ticks)
+
+    Raises:
+        ValueError: An argument has the correct type, but an incorrect value
+    """
+    if shift_ticks < 0:
+        raise ValueError(f"Expected shift_ticks ≥ 0, got {shift_ticks}")
+    if phase1_ticks < 0:
+        raise ValueError(f"Expected phase1_ticks ≥ 0, got {phase1_ticks}")
+    if phase2_ticks < 0:
+        raise ValueError(f"Expected phase2_ticks ≥ 0, got {phase2_ticks}")
+
+    # T = 0
+    clock.next = init
+    await sleep(shift_ticks)
+
+    while True:
+        clock.next = ~clock.value
+        await sleep(phase1_ticks)
+
+        clock.next = ~clock.value
+        await sleep(phase2_ticks)
+
+
+async def dff_arn_drv(
+    q: SimVar,
+    d: Callable[[], logicvec],
+    reset_n: TraceVar,
+    reset_value: logicvec,
+    clock: TraceVar,
+):
+    while True:
+        var = await notify(reset_n.negedge, clock.posedge)
+        assert var in {reset_n, clock}
+        if var is reset_n:
+            q.next = reset_value
+        elif var is clock and reset_n.value is logic.T:
+            q.next = d()
---
+++ tests/test_fsm.py
@@ -0,0 +1,220 @@
+"""
+Example FSM implementation
+
+Demonstrate usage of an enum
+"""
+
+from enum import Enum
+
+from seqlogic.logic import logic
+from seqlogic.logicvec import vec
+from seqlogic.sim import SimVar, get_loop, notify
+
+from .common import TraceVar, clock_drv, dff_arn_drv, reset_drv, waves
+
+loop = get_loop()
+
+
+class SeqDetect(Enum):
+    A = vec("2b00")
+    B = vec("2b01")
+    C = vec("2b10")
+    D = vec("2b11")
+
+    XX = vec("2bxx")
+
+
+class TraceSeqDetect(SimVar):
+    """
+    Variable that supports dumping to memory.
+    """
+
+    def __init__(self):
+        super().__init__(value=SeqDetect.XX)
+        waves[self._sim.time()][self] = self._value
+
+    def update(self):
+        if self.dirty():
+            waves[self._sim.time()][self] = self._next
+        super().update()
+
+
+async def input_drv(
+    x: TraceVar,
+    reset_n: TraceVar,
+    clock: TraceVar,
+):
+    await notify(reset_n.negedge)
+    x.next = logic.F
+
+    await notify(reset_n.posedge)
+    await notify(clock.posedge)
+    await notify(clock.posedge)
+    x.next = logic.T  # A => B
+
+    await notify(clock.posedge)
+    x.next = logic.T  # B => C
+
+    await notify(clock.posedge)
+    x.next = logic.T  # C => D
+
+    await notify(clock.posedge)
+    x.next = logic.T  # D => D
+
+    await notify(clock.posedge)
+    x.next = logic.F  # D => A
+
+
+def test_fsm():
+    """Test a 3-bit LFSR"""
+    loop.reset()
+    waves.clear()
+
+    # State Variables
+    ps = TraceSeqDetect()
+
+    # Inputs
+    x = TraceVar()
+
+    def ns() -> SeqDetect:
+        match (ps.value, x.value):
+            case (SeqDetect.A, logic.F):
+                return SeqDetect.A
+            case (SeqDetect.A, logic.T):
+                return SeqDetect.B
+
+            case (SeqDetect.B, logic.F):
+                return SeqDetect.A
+            case (SeqDetect.B, logic.T):
+                return SeqDetect.C
+
+            case (SeqDetect.C, logic.F):
+                return SeqDetect.A
+            case (SeqDetect.C, logic.T):
+                return SeqDetect.D
+
+            case (SeqDetect.D, logic.F):
+                return SeqDetect.A
+            case (SeqDetect.D, logic.T):
+                return SeqDetect.D
+
+            case _:
+                return SeqDetect.XX
+
+    # Control Variables
+    reset_n = TraceVar()
+    reset_value = SeqDetect.A
+    clock = TraceVar()
+
+    # Schedule input
+    loop.add_proc(input_drv, 0, x, reset_n, clock)
+
+    # Schedule LFSR
+    loop.add_proc(dff_arn_drv, 0, ps, ns, reset_n, reset_value, clock)
+
+    # Schedule reset and clock
+    # Note: Avoiding simultaneous reset/clock negedge/posedge on purpose
+    loop.add_proc(reset_drv, 1, reset_n, init=logic.T, phase1_ticks=6, phase2_ticks=10)
+    loop.add_proc(clock_drv, 1, clock, init=logic.F, shift_ticks=5, phase1_ticks=5, phase2_ticks=5)
+
+    loop.run(until=100)
+
+    exp = {
+        # Initialize everything to X'es
+        -1: {
+            reset_n: logic.X,
+            clock: logic.X,
+            x: logic.X,
+            ps: SeqDetect.XX,
+        },
+        0: {
+            reset_n: logic.T,
+            clock: logic.F,
+        },
+        # clock.posedge; reset_n = 1
+        5: {
+            clock: logic.T,
+        },
+        # reset_n.negedge
+        6: {
+            reset_n: logic.F,
+            x: logic.F,
+            ps: SeqDetect.A,
+        },
+        10: {
+            clock: logic.F,
+        },
+        # clock.posedge; reset_n = 0
+        15: {
+            clock: logic.T,
+        },
+        # reset_n.posedge
+        16: {
+            reset_n: logic.T,
+        },
+        20: {
+            clock: logic.F,
+        },
+        # clock.posedge; reset_n = 1
+        25: {
+            clock: logic.T,
+        },
+        30: {
+            clock: logic.F,
+        },
+        # clock.posedge
+        35: {
+            clock: logic.T,
+            x: logic.T,
+        },
+        40: {
+            clock: logic.F,
+        },
+        # clock.posedge
+        45: {
+            clock: logic.T,
+            ps: SeqDetect.B,
+        },
+        50: {
+            clock: logic.F,
+        },
+        # clock.posedge
+        55: {
+            clock: logic.T,
+            ps: SeqDetect.C,
+        },
+        60: {
+            clock: logic.F,
+        },
+        # clock.posedge
+        65: {
+            clock: logic.T,
+            ps: SeqDetect.D,
+        },
+        70: {
+            clock: logic.F,
+        },
+        # clock.posedge
+        75: {
+            clock: logic.T,
+            x: logic.F,
+            # ps: D => D
+        },
+        80: {
+            clock: logic.F,
+        },
+        # clock.posedge
+        85: {
+            clock: logic.T,
+            ps: SeqDetect.A,
+        },
+        90: {
+            clock: logic.F,
+        },
+        # clock.posedge
+        95: {
+            clock: logic.T,
+        },
+    }
+
+    assert waves == exp
