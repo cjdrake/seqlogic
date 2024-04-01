2c8753b237ec74d6497d5b9a70619982f5653842
Chris Drake <cjdrake@gmail.com>
2024-04-01 01:34:07 -0400

    Cleanup hier module

--- src/seqlogic/hier.py
+++ src/seqlogic/hier.py
@@ -1,4 +1,10 @@
-"""Logic Design Hierarchy."""
+"""Logic Design Hierarchy.
+
+Implement the fundamental components of an N-ary tree.
+A tree has branches and leaves.
+Leaves must have a parent.
+A branch may either have a parent, or no parent (the root).
+"""
 
 from __future__ import annotations
 
@@ -9,8 +15,13 @@ from collections.abc import Generator
 class Hierarchy(ABC):
     """Any hierarchical design element."""
 
-    def __init__(self, name: str, parent: Hierarchy | None):
-        """TODO(cjdrake): Write docstring."""
+    def __init__(self, name: str, parent: Branch | None):
+        """Initialize.
+
+        Args:
+            name: Name of this tree node.
+            parent: Parent tree node, or None.
+        """
         self._name = name
         self._parent = parent
 
@@ -20,7 +31,7 @@ class Hierarchy(ABC):
         return self._name
 
     @property
-    def parent(self) -> Hierarchy | None:
+    def parent(self) -> Branch | None:
         """Return the parent, or None."""
         return self._parent
 
@@ -42,55 +53,53 @@ class Branch(Hierarchy):
     """Design hierarchy branch node."""
 
     def __init__(self, name: str, parent: Branch | None = None):
-        """TODO(cjdrake): Write docstring."""
         super().__init__(name, parent)
-        self._children: list[Hierarchy] = []
+        self._children: list[Branch | Leaf] = []
         if parent is not None:
             parent.add_child(self)
 
     @property
     def qualname(self) -> str:
-        """Return the branch's fully qualified name."""
         if self._parent is None:
             return f"/{self._name}"
         else:
             return f"{self._parent.qualname}/{self._name}"
 
     def iter_bfs(self) -> Generator[Hierarchy, None, None]:
-        """TODO(cjdrake): Write docstring."""
         yield self
         for child in self._children:
             yield from child.iter_bfs()
 
     def iter_dfs(self) -> Generator[Hierarchy, None, None]:
-        """TODO(cjdrake): Write docstring."""
         for child in self._children:
             yield from child.iter_dfs()
         yield self
 
-    def add_child(self, child: Hierarchy):
+    def add_child(self, child: Branch | Leaf):
         """Add child branch or leaf."""
         self._children.append(child)
+
+    def is_root(self) -> bool:
+        """Return True if this branch is the root of the tree."""
+        return self._parent is None
 
 
 class Leaf(Hierarchy):
     """Design hierarchy leaf node."""
 
     def __init__(self, name: str, parent: Branch):
-        """TODO(cjdrake): Write docstring."""
         super().__init__(name, parent)
         parent.add_child(self)
 
+        # Help type checker verify parent is not None
+        self._parent: Branch
+
     @property
     def qualname(self) -> str:
-        """Return the leaf's fully qualified name."""
-        assert self._parent is not None
         return f"{self._parent.qualname}/{self._name}"
 
-    def iter_bfs(self) -> Generator[Leaf, None, None]:
-        """TODO(cjdrake): Write docstring."""
+    def iter_bfs(self) -> Generator[Hierarchy, None, None]:
         yield self
 
-    def iter_dfs(self) -> Generator[Leaf, None, None]:
-        """TODO(cjdrake): Write docstring."""
+    def iter_dfs(self) -> Generator[Hierarchy, None, None]:
         yield self
