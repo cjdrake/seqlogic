2e17ac0dca5bcf8f78228fd23affdbf90c6c8ab6
Chris Drake <cjdrake@gmail.com>
2024-05-25 00:04:16 -0400

    Simplify

--- tests/riscv/core/regfile.py
+++ tests/riscv/core/regfile.py
@@ -1,10 +1,8 @@
 """Register File."""
 
-from seqlogic import Array, Bit, Bits, Module, changed, clog2, resume
-from seqlogic.lbool import Vec, zeros
+from seqlogic import Array, Bit, Bits, Module, changed, resume
+from seqlogic.lbool import Vec
 from seqlogic.sim import active, reactive
-
-DEPTH = 32
 
 
 class RegFile(Module):
@@ -12,17 +10,16 @@ class RegFile(Module):
 
     def __init__(self, name: str, parent: Module | None):
         super().__init__(name, parent)
-        self._addr_bits = clog2(DEPTH)
         self._build()
 
     def _build(self):
         # Ports
         self.wr_en = Bit(name="wr_en", parent=self)
-        self.wr_addr = Bits(name="wr_addr", parent=self, dtype=Vec[self._addr_bits])
+        self.wr_addr = Bits(name="wr_addr", parent=self, dtype=Vec[5])
         self.wr_data = Bits(name="wr_data", parent=self, dtype=Vec[32])
-        self.rs1_addr = Bits(name="rs1_addr", parent=self, dtype=Vec[self._addr_bits])
+        self.rs1_addr = Bits(name="rs1_addr", parent=self, dtype=Vec[5])
         self.rs1_data = Bits(name="rs1_data", parent=self, dtype=Vec[32])
-        self.rs2_addr = Bits(name="rs2_addr", parent=self, dtype=Vec[self._addr_bits])
+        self.rs2_addr = Bits(name="rs2_addr", parent=self, dtype=Vec[5])
         self.rs2_data = Bits(name="rs2_data", parent=self, dtype=Vec[32])
         self.clock = Bit(name="clock", parent=self)
         self.reset = Bit(name="reset", parent=self)
@@ -38,14 +35,14 @@ class RegFile(Module):
         while True:
             state = await resume((self.reset, self.reset.is_posedge), (self.clock, f))
             if state is self.reset:
-                for i in range(DEPTH):
+                for i in range(32):
                     self._regs[i].next = "32h0000_0000"
             elif state is self.clock:
                 addr = self.wr_addr.value
                 # If wr_en=1, address must be known
                 assert not addr.has_unknown()
                 # Write to address zero has no effect
-                if addr != zeros(self._addr_bits):
+                if addr != "5b0_0000":
                     self._regs[addr].next = self.wr_data.value
             else:
                 assert False
