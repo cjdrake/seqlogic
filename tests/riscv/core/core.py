e47db39958f6b10503862db6bc7e73c8658634f6
Chris Drake <cjdrake@gmail.com>
2024-08-04 22:20:26 -0400

    Simplify a few getattr operators

--- tests/riscv/core/core.py
+++ tests/riscv/core/core.py
@@ -49,9 +49,9 @@ class Core(Module):
             name="ctlpath",
             mod=CtlPath,
         ).connect(
-            opcode=(lambda x: x.opcode, inst),
-            funct3=(lambda x: x.funct3, inst),
-            funct7=(lambda x: x.funct7, inst),
+            opcode=inst.opcode,
+            funct3=inst.funct3,
+            funct7=inst.funct7,
             alu_result_eq_zero=alu_result_eq_zero,
             pc_wr_en=pc_wr_en,
             reg_wr_en=reg_wr_en,
@@ -89,7 +89,7 @@ class Core(Module):
             name="data_mem_if",
             mod=DataMemIf,
         ).connect(
-            data_format=(lambda x: x.funct3, inst),
+            data_format=inst.funct3,
             addr=addr,
             wr_en=wr_en,
             wr_data=wr_data,
