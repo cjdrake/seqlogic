9b5f8e6145351e69e6e9c204c1c78f72a7874687
Chris Drake <cjdrake@gmail.com>
2024-08-12 01:22:35 -0400

    Modernize LFSR test

--- tests/test_lfsr.py
+++ tests/test_lfsr.py
@@ -8,9 +8,11 @@ from collections import defaultdict
 from seqlogic import Module, Vec, cat, get_loop
 from seqlogic.control.globals import drive_clock, drive_reset
 
-from .common import p_dff
-
 loop = get_loop()
+
+
+def lfsr(x: Vec[3]) -> Vec[3]:
+    return cat(x[0] ^ x[2], x[:2])
 
 
 class Top(Module):
@@ -19,10 +21,19 @@ class Top(Module):
     def __init__(self):
         super().__init__(name="top", parent=None)
         # Control
-        self.input(name="reset_n", dtype=Vec[1])
-        self.input(name="clock", dtype=Vec[1])
+        reset_n = self.input(name="reset_n", dtype=Vec[1])
+        clock = self.input(name="clock", dtype=Vec[1])
+
         # State
-        self.logic(name="q", dtype=Vec[3])
+        q = self.logic(name="q", dtype=Vec[3])
+        d = self.logic(name="d", dtype=Vec[3])
+
+        # Invert the reset
+        reset = self.logic(name="reset", dtype=Vec[1])
+        self.expr(reset, ~reset_n)
+
+        self.combi(d, lfsr, q)
+        self.dff_ar(q, d, clock, reset, "3b100")
 
 
 def test_lfsr():
@@ -30,18 +41,13 @@ def test_lfsr():
     loop.reset()
 
     top = Top()
+    top.elab()
+
     waves = defaultdict(dict)
-    top.dump_waves(waves, r".*")
+    top.dump_waves(waves, r"/top/q")
 
     assert top._q.name == "q"
     assert top._q.qualname == "/top/q"
-
-    def d():
-        v = top._q.value
-        return cat(v[0] ^ v[2], v[:2])
-
-    # Schedule LFSR
-    loop.add_active(p_dff(top._q, d, top.clock, top.reset_n, "3b100"))
 
     # Schedule reset and clock
     # Note: Avoiding simultaneous reset/clock negedge/posedge on purpose
@@ -52,96 +58,19 @@ def test_lfsr():
 
     exp = {
         # Initialize everything to X'es
-        -1: {
-            top.reset_n: "1bX",
-            top.clock: "1bX",
-            top._q: "3bXXX",
-        },
-        0: {
-            top.reset_n: "1b1",
-            top.clock: "1b0",
-        },
-        # clock.posedge; reset_n = 1
-        # q = xxx
-        5: {
-            top.clock: "1b1",
-        },
+        -1: {top._q: "3bXXX"},
         # reset_n.negedge
-        # q = reset_value
-        6: {
-            top.reset_n: "1b0",
-            top._q: "3b100",
-        },
-        10: {
-            top.clock: "1b0",
-        },
-        # clock.posedge; reset_n = 0
-        15: {
-            top.clock: "1b1",
-        },
-        # reset_n.posedge
-        16: {
-            top.reset_n: "1b1",
-        },
-        20: {
-            top.clock: "1b0",
-        },
+        6: {top._q: "3b100"},
         # clock.posedge; reset_n = 1
-        # q = 001
-        25: {
-            top.clock: "1b1",
-            top._q: "3b001",
-        },
-        30: {
-            top.clock: "1b0",
-        },
-        35: {
-            top.clock: "1b1",
-            top._q: "3b011",
-        },
-        40: {
-            top.clock: "1b0",
-        },
-        45: {
-            top.clock: "1b1",
-            top._q: "3b111",
-        },
-        50: {
-            top.clock: "1b0",
-        },
-        55: {
-            top.clock: "1b1",
-            top._q: "3b110",
-        },
-        60: {
-            top.clock: "1b0",
-        },
-        65: {
-            top.clock: "1b1",
-            top._q: "3b101",
-        },
-        70: {
-            top.clock: "1b0",
-        },
-        75: {
-            top.clock: "1b1",
-            top._q: "3b010",
-        },
-        80: {
-            top.clock: "1b0",
-        },
+        25: {top._q: "3b001"},
+        35: {top._q: "3b011"},
+        45: {top._q: "3b111"},
+        55: {top._q: "3b110"},
+        65: {top._q: "3b101"},
+        75: {top._q: "3b010"},
         # Repeat cycle
-        85: {
-            top.clock: "1b1",
-            top._q: "3b100",
-        },
-        90: {
-            top.clock: "1b0",
-        },
-        95: {
-            top.clock: "1b1",
-            top._q: "3b001",
-        },
+        85: {top._q: "3b100"},
+        95: {top._q: "3b001"},
     }
 
     assert waves == exp
