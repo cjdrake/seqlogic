01f2596e0378337b66ce892e92255695fa443bf6
Chris Drake <cjdrake@gmail.com>
2024-09-08 00:20:30 -0400

    Implement a half bandwidth elastic buffer (HBEB)

---
+++ src/seqlogic/datastruct/hbeb.py
@@ -0,0 +1,49 @@
+"""Half bandwidth elastic buffer."""
+
+# PyLint thinks some Packed types are Unpacked for some reason
+# pylint: disable=invalid-unary-operand-type
+
+
+from seqlogic import Module, Vec
+
+
+class Hbeb(Module):
+    """Half bandwidth elastic buffer."""
+
+    T: type = Vec[8]
+
+    def build(self):
+        # Ports
+        rd_ready = self.input(name="rd_ready", dtype=Vec[1])
+        rd_valid = self.output(name="rd_valid", dtype=Vec[1])
+        rd_data = self.output(name="rd_data", dtype=self.T)
+
+        wr_ready = self.output(name="wr_ready", dtype=Vec[1])
+        wr_valid = self.input(name="wr_valid", dtype=Vec[1])
+        wr_data = self.input(name="wr_data", dtype=self.T)
+
+        clock = self.input(name="clock", dtype=Vec[1])
+        reset = self.input(name="reset", dtype=Vec[1])
+
+        full = self.logic(name="full", dtype=Vec[1])
+        full_next = self.logic(name="full_next", dtype=Vec[1])
+
+        rd_en = self.logic(name="rd_en", dtype=Vec[1])
+        wr_en = self.logic(name="wr_en", dtype=Vec[1])
+
+        buf = self.logic(name="buf", dtype=self.T)
+
+        # Convert ready/valid to FIFO
+        self.assign(rd_valid, full)
+        self.expr(rd_en, rd_ready & rd_valid)
+
+        self.expr(wr_ready, ~full)
+        self.expr(wr_en, wr_ready & wr_valid)
+
+        # Control
+        self.expr(full_next, full & ~rd_en | ~full & wr_en)
+        self.dff_r(full, full_next, clock, reset, rval="1b0")
+
+        # Data
+        self.assign(rd_data, buf)
+        self.dff_en(buf, wr_data, wr_en, clock)
---
+++ tests/datastruct/test_hbeb.py
@@ -0,0 +1,184 @@
+"""Test Pipe Register."""
+
+import os
+from collections import deque
+from random import randint
+
+from vcd import VCDWriter
+
+from seqlogic import Module, Struct, Vec, finish, get_loop, resume, sleep
+from seqlogic.control.globals import drv_clock, drv_reset
+from seqlogic.datastruct.hbeb import Hbeb
+
+loop = get_loop()
+
+DIR = os.path.dirname(__file__)
+
+
+class MyStruct(Struct):
+    a: Vec[4]
+    b: Vec[4]
+    c: Vec[4]
+    d: Vec[4]
+
+
+class Top(Module):
+    """Testbench Top."""
+
+    N: int = 100
+    T: type = MyStruct
+
+    def build(self):
+        rd_ready = self.logic(name="rd_ready", dtype=Vec[1])
+        rd_valid = self.logic(name="rd_valid", dtype=Vec[1])
+        rd_data = self.logic(name="rd_data", dtype=self.T)
+
+        wr_ready = self.logic(name="wr_ready", dtype=Vec[1])
+        wr_valid = self.logic(name="wr_valid", dtype=Vec[1])
+        wr_data = self.logic(name="wr_data", dtype=self.T)
+
+        clock = self.logic(name="clock", dtype=Vec[1])
+        reset = self.logic(name="reset", dtype=Vec[1])
+
+        self.submod(
+            name="dut",
+            mod=Hbeb,
+            T=self.T,
+        ).connect(
+            rd_ready=rd_ready,
+            rd_valid=rd_valid,
+            rd_data=rd_data,
+            wr_ready=wr_ready,
+            wr_valid=wr_valid,
+            wr_data=wr_data,
+            clock=clock,
+            reset=reset,
+        )
+
+        self.drv(drv_clock(clock, shiftticks=1))
+        self.drv(drv_reset(reset, shiftticks=2, onticks=2))
+        self.drv(self.drv_wr())
+        self.drv(self.drv_rd())
+
+        self.mon(self.mon_wr())
+        self.mon(self.mon_rd())
+
+        self.wdata = deque()
+        self.rdata = deque()
+
+    async def drv_wr(self):
+        clk, rst = self._clock, self._reset
+        wr_ready, wr_valid = self._wr_ready, self._wr_valid
+        wr_data = self._wr_data
+
+        # reset: __/‾‾
+        await rst.posedge()
+        wr_valid.next = "1b0"
+
+        # reset: ‾‾\__
+        await rst.negedge()
+
+        # Delay a few cycles
+        for _ in range(5):
+            await clk.posedge()
+
+        def pred():
+            return clk.is_posedge() and wr_ready.value == "1b1"
+
+        for _ in range(self.N):
+            wr_valid.next = "1b1"
+            wr_data.next = self.T.rand()
+            await resume((clk, pred))
+
+            for _ in range(randint(0, 5)):
+                wr_valid.next = "1b0"
+                wr_data.next = self.T.xes()
+                await clk.posedge()
+
+        await sleep(10)
+        finish()
+
+    async def drv_rd(self):
+        clk, rst = self._clock, self._reset
+        rd_ready, rd_valid = self._rd_ready, self._rd_valid
+
+        # reset: __/‾‾
+        await rst.posedge()
+        rd_ready.next = "1b0"
+
+        # reset: ‾‾\__
+        await rst.negedge()
+
+        # Delay a few cycles
+        for _ in range(5):
+            await clk.posedge()
+
+        def pred():
+            return clk.is_posedge() and rd_valid.value == "1b1"
+
+        while True:
+            rd_ready.next = "1b1"
+            await resume((clk, pred))
+
+            for _ in range(randint(0, 5)):
+                rd_ready.next = "1b0"
+                await clk.posedge()
+
+    async def mon_wr(self):
+        clk, rst = self._clock, self._reset
+        wr_ready, wr_valid = self._wr_ready, self._wr_valid
+        data = self._wr_data
+
+        def pred():
+            return (
+                clk.is_posedge()
+                and rst.is_neg()  # noqa: W503
+                and wr_ready.value == "1b1"  # noqa: W503
+                and wr_valid.value == "1b1"  # noqa: W503
+            )
+
+        while True:
+            await resume((clk, pred))
+            self.wdata.append(data.value)
+
+    async def mon_rd(self):
+        clk, rst = self._clock, self._reset
+        rd_ready, rd_valid = self._rd_ready, self._rd_valid
+        data = self._rd_data
+
+        def pred():
+            return (
+                clk.is_posedge()
+                and rst.is_neg()  # noqa: W503
+                and rd_ready.value == "1b1"  # noqa: W503
+                and rd_valid.value == "1b1"  # noqa: W503
+            )
+
+        while True:
+            await resume((clk, pred))
+            self.rdata.append(data.value)
+
+            exp = self.wdata.popleft()
+            got = self.rdata.popleft()
+            assert got == exp
+
+
+def test_hbeb():
+    vcd = os.path.join(DIR, "hbeb.vcd")
+    with (
+        open(vcd, "w", encoding="utf-8") as f,
+        VCDWriter(f, timescale="1ns") as vcdw,
+    ):
+        loop.reset()
+
+        # Instantiate top
+        top = Top(name="top")
+
+        # Register design w/ event loop
+        top.elab()
+
+        # Dump all signals to VCD
+        top.dump_vcd(vcdw, ".*")
+
+        # Do the damn thing
+        loop.run()
