25f3d63c3e6f06259a039ae778b811e9771140c4
Chris Drake <cjdrake@gmail.com>
2024-10-17 21:54:29 -0400

    Update sphinx configuration

--- doc/source/conf.py
+++ doc/source/conf.py
@@ -1,28 +1,28 @@
-# Configuration file for the Sphinx documentation builder.
-#
-# For the full list of built-in configuration values, see the documentation:
-# https://www.sphinx-doc.org/en/master/usage/configuration.html
+"""
+Configuration file for the Sphinx documentation builder.
+
+For the full list of built-in configuration values, see the documentation:
+https://www.sphinx-doc.org/en/master/usage/configuration.html
+"""
 
 # -- Project information -----------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
 
-project = 'Sequential Logic'
-copyright = '2024, Chris Drake'
-author = 'Chris Drake'
-release = '0.29.0'
+project = "Sequential Logic"
+copyright = "2024, Chris Drake"  # pylint: disable=redefined-builtin
+author = "Chris Drake"
+release = "0.29.0"
 
 # -- General configuration ---------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
 
 extensions = []
 
-templates_path = ['_templates']
+templates_path = ["_templates"]
 exclude_patterns = []
-
-
 
 # -- Options for HTML output -------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
 
-html_theme = 'alabaster'
-html_static_path = ['_static']
+html_theme = "alabaster"
+html_static_path = ["_static"]
