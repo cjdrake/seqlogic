d37b9ca0aa658e49686bcb6f2b3af1a67d64ceff
Chris Drake <cjdrake@gmail.com>
2024-10-20 22:02:29 -0400

    fixup

--- doc/source/conf.py
+++ doc/source/conf.py
@@ -5,13 +5,20 @@ For the full list of built-in configuration values, see the documentation:
 https://www.sphinx-doc.org/en/master/usage/configuration.html
 """
 
+import tomllib
+from pathlib import Path
+
+WORKSPACE = Path("../..").resolve()
+with open(WORKSPACE / "pyproject.toml", mode="rb") as f:
+    proj_toml = tomllib.load(f)
+
 # -- Project information -----------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
 
 project = "Sequential Logic"
 copyright = "2024, Chris Drake"  # pylint: disable=redefined-builtin
 author = "Chris Drake"
-release = "0.29.0"
+release = proj_toml["project"]["version"]
 
 # -- General configuration ---------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
