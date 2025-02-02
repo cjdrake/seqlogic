791359ce8f67bfdcf63b4847bd8c92f0b5b50c76
Chris Drake <cjdrake@gmail.com>
2025-02-01 21:32:46 -0500

    Simplify clz implementation

--- src/seqlogic/algorithm/count.py
+++ src/seqlogic/algorithm/count.py
@@ -5,11 +5,7 @@ from seqlogic import Bits, Vec, cat, encode_onehot, ngc, pack
 
 def clz(x: Bits) -> Vec:
     """Count leading zeros."""
-    xr = pack(x)
-    # Decode: {0000: 10000, 0001: 01000, ..., 01--: 00010, 1---: 00001}
-    d = cat(xr, True) & ngc(xr)
-    # Encode {10000: 100, 01000: 011, 00100: 010, 00010: 001, 00001: 000}
-    return encode_onehot(d)
+    return ctz(pack(x))
 
 
 def ctz(x: Bits) -> Vec:
